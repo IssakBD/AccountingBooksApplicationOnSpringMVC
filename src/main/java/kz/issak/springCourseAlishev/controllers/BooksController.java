eb9607cf4a8781ceffc2d79014cf9ddd990992da
admin <administrator@jits-3-28.jits.ipa.dc>
2022-06-18 21:23:23 +0600

    added feature to create, update, delete, listing people without database

---
+++ src/main/java/kz/issak/springCourseAlishev/controllers/BooksController.java
@@ -0,0 +1,9 @@
+//package kz.issak.springCourseAlishev.controllers;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//
+////@Controller
+//@RequestMapping("/books")
+//public class BooksController {
+//}
