ab5c35443cc479b5669c103ae8c0e1be4b8d1449
admin <administrator@jits-3-28.jits.ipa.dc>
2022-06-20 13:31:48 +0600

    add database and realise features with database of ArrayList

--- src/main/java/kz/issak/springCourseAlishev/dao/BooksDAO.java
+++ src/main/java/kz/issak/springCourseAlishev/dao/BooksDAO.java
@@ -1,45 +1,152 @@
 package kz.issak.springCourseAlishev.dao;
 
 import kz.issak.springCourseAlishev.models.Book;
+import kz.issak.springCourseAlishev.models.Person;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class BooksDAO {
-    private List<Book> books;
-    private static int BOOK_COUNTER;
+    //Выполнение на arrayList-е вместо ДБ
+//    private List<Book> books;
+//    private static int BOOK_COUNTER;
 
-    {
-        books = new ArrayList<>();
+//    {
+//        books = new ArrayList<>();
+//
+//        books.add(new Book(++BOOK_COUNTER, "Baglan Issak Daurenbekuly", "Baglan", 1996));
+//        books.add(new Book(++BOOK_COUNTER, "Symbat Issak Kassymkyzy", "Symbat",1996));
+//        books.add(new Book(++BOOK_COUNTER, "Amir Issak Baglanuly", "Amir",2020));
+//        books.add(new Book(++BOOK_COUNTER, "Hadisha Issak Baglankyzy", "Amir", 2022));
+//    }
 
-        books.add(new Book(++BOOK_COUNTER, "Baglan Issak Daurenbekuly", "Baglan", 1996));
-        books.add(new Book(++BOOK_COUNTER, "Symbat Issak Kassymkyzy", "Symbat",1996));
-        books.add(new Book(++BOOK_COUNTER, "Amir Issak Baglanuly", "Amir",2020));
-        books.add(new Book(++BOOK_COUNTER, "Hadisha Issak Baglankyzy", "Amir", 2022));
+    //С помощью JDBC API {
+    private static final String URL = "jdbc:postgresql://localhost:5432/project1";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection; //Соединение с БД
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver"); //С помощью рефлексии подгружаем класс с нашим JDBC driver, в последних версиях не требуется это делать
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); //Подключаемся к БД с помощью драйвера
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+//}
+
     public List<Book> getBooksList(){
+        //Выполнение на arrayList-е вместо ДБ
+        //return books;
+
+        //С помощью JDBC API
+        List<Book> books = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement(); //на нашем соединении создаем запрос в БД
+            String SQL = "SELECT * FROM Book";
+            ResultSet resultSet = statement.executeQuery(SQL); //Выполняет запрос на нашей БД. ResultSet - объект который инкапсулирует результат запроса к БД.
+            while (resultSet.next()){
+                Book book = new Book();
+
+                book.setId(resultSet.getInt("id")); //Извлекаем и сетим данные в person из resultSet
+                book.setName(resultSet.getString("name"));
+                book.setAuthorName(resultSet.getString("authorName"));
+                book.setDateOfPublish(resultSet.getInt("dateOfPublish"));
+
+                books.add(book);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return books;
     }
 
     public Book getBook(int id){
-        return books.stream().filter(book -> book.getId() == id).findAny().orElse(null);
+        //Выполнение на arrayList-е вместо ДБ
+        //return books.stream().filter(book -> book.getId() == id).findAny().orElse(null);
+
+        //С помощью JDBC API
+        Book book = new Book();
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Book WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            book.setId(resultSet.getInt("id"));
+            book.setName(resultSet.getString("name"));
+            book.setAuthorName(resultSet.getString("authorName"));
+            book.setDateOfPublish(resultSet.getInt("dateOfPublish"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return book;
     }
 
     public void addBook(Book book){
-        book.setId(++BOOK_COUNTER);
-        books.add(book);
+        //Выполнение на arrayList-е вместо ДБ
+//        book.setId(++BOOK_COUNTER);
+//        books.add(book);
+
+        //Изначально делал на statement вместо preparedStatement. Statement медленнее и опаснее чем PreparedStatement.
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Book(name, authorName, dateOfPublish) VALUES (?, ?, ?)");
+            preparedStatement.setString(1, book.getName());
+            preparedStatement.setString(2, book.getAuthorName());
+            preparedStatement.setInt(3, book.getDateOfPublish());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void editBook(int id, Book updatedBook){
-        Book bookToBeEdited = getBook(id);
-        bookToBeEdited.setAuthorName(updatedBook.getAuthorName());
-        bookToBeEdited.setName(updatedBook.getName());
-        bookToBeEdited.setDateOfPublish(updatedBook.getDateOfPublish());
+        //Выполнение на arrayList-е вместо ДБ
+//        Book bookToBeEdited = getBook(id);
+//        bookToBeEdited.setAuthorName(updatedBook.getAuthorName());
+//        bookToBeEdited.setName(updatedBook.getName());
+//        bookToBeEdited.setDateOfPublish(updatedBook.getDateOfPublish());
+
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Book SET name = ?, authorName = ?, dateOfPublish = ? WHERE id = ?");
+            preparedStatement.setString(1, updatedBook.getName());
+            preparedStatement.setString(2, updatedBook.getAuthorName());
+            preparedStatement.setInt(3, updatedBook.getDateOfPublish());
+            preparedStatement.setInt(4, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void deleteBook(int id){
-        books.removeIf(p -> p.getId() == id);
+        //Выполнение на arrayList-е вместо ДБ
+//        books.removeIf(p -> p.getId() == id);
+
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Book WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/kz/issak/springCourseAlishev/dao/PeopleDAO.java
+++ src/main/java/kz/issak/springCourseAlishev/dao/PeopleDAO.java
@@ -3,43 +3,146 @@ package kz.issak.springCourseAlishev.dao;
 import kz.issak.springCourseAlishev.models.Person;
 import org.springframework.stereotype.Repository;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 @Repository
 public class PeopleDAO {
-    private List<Person> people;
-    private static int PEOPLE_COUNTER;
 
-    {
-        people = new ArrayList<>();
+    //Выполнение на arrayList-е вместо ДБ
+//    private List<Person> people;
+//    private static int PEOPLE_COUNTER;
+//
+//    {
+//        people = new ArrayList<>();
+//
+//        people.add(new Person(++PEOPLE_COUNTER, "Baglan Issak Daurenbekuly", "24.09.1996"));
+//        people.add(new Person(++PEOPLE_COUNTER, "Symbat Issak Kassymkyzy", "25.06.1996"));
+//        people.add(new Person(++PEOPLE_COUNTER, "Amir Issak Baglanuly", "21.07.2020"));
+//        people.add(new Person(++PEOPLE_COUNTER, "Hadisha Issak Baglankyzy", "20.01.2022"));
+//    }
 
-        people.add(new Person(++PEOPLE_COUNTER, "Baglan Issak Daurenbekuly", "24.09.1996"));
-        people.add(new Person(++PEOPLE_COUNTER, "Symbat Issak Kassymkyzy", "25.06.1996"));
-        people.add(new Person(++PEOPLE_COUNTER, "Amir Issak Baglanuly", "21.07.2020"));
-        people.add(new Person(++PEOPLE_COUNTER, "Hadisha Issak Baglankyzy", "20.01.2022"));
+    //  С помощью JDBC API {
+    private static final String URL = "jdbc:postgresql://localhost:5432/project1";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
+
+    private static Connection connection; //Соединение с БД
+
+    static {
+        try {
+            Class.forName("org.postgresql.Driver"); //С помощью рефлексии подгружаем класс с нашим JDBC driver, в последних версиях не требуется это делать
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); //Подключаемся к БД с помощью драйвера
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    //}
     public List<Person> getPeopleList(){
+        //Выполнение на arrayList-е вместо ДБ
+        //return people;
+
+
+        //С помощью JDBC API
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement(); //на нашем соединении создаем запрос в БД
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL); //Выполняет запрос на нашей БД. ResultSet - объект который инкапсулирует результат запроса к БД.
+            while (resultSet.next()){
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id")); //Извлекаем и сетим данные в person из resultSet
+                person.setFullName(resultSet.getString("fullName"));
+                person.setDateOfBirth(resultSet.getString("dateOfBirth"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return people;
     }
 
     public Person getPerson(int id){
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        //Выполнение на arrayList-е вместо ДБ
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+
+        //С помощью JDBC API
+        Person person = new Person();
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM Person WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            person.setId(resultSet.getInt("id"));
+            person.setFullName(resultSet.getString("fullName"));
+            person.setDateOfBirth(resultSet.getString("dateOfBirth"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return person;
     }
 
     public void addPerson(Person person){
-        person.setId(++PEOPLE_COUNTER);
-        people.add(person);
+        //Выполнение на arrayList-е вместо ДБ
+//        person.setId(++PEOPLE_COUNTER);
+//        people.add(person);
+
+        //Изначально делал на statement вместо preparedStatement. Statement медленнее и опаснее чем PreparedStatement.
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("INSERT INTO Person(fullName, dateOfBirth) VALUES (?, ?)");
+            preparedStatement.setString(1, person.getFullName());
+            preparedStatement.setString(2, person.getDateOfBirth());
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void editPerson(int id, Person updatedPerson){
-        Person personToBeEdited = getPerson(id);
-        personToBeEdited.setFullName(updatedPerson.getFullName());
-        personToBeEdited.setDateOfBirth(updatedPerson.getDateOfBirth());
+        //Выполнение на arrayList-е вместо ДБ
+//        Person personToBeEdited = getPerson(id);
+//        personToBeEdited.setFullName(updatedPerson.getFullName());
+//        personToBeEdited.setDateOfBirth(updatedPerson.getDateOfBirth());
+
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("UPDATE Person SET fullName = ?, dateOfBirth = ? WHERE id = ?");
+            preparedStatement.setString(1, updatedPerson.getFullName());
+            preparedStatement.setString(2, updatedPerson.getDateOfBirth());
+            preparedStatement.setInt(3, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void deletePerson(int id){
-        people.removeIf(p -> p.getId() == id);
+        //Выполнение на arrayList-е вместо ДБ
+        //people.removeIf(p -> p.getId() == id);
+
+        //С помощью JDBC API
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
